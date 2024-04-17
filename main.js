dcd36fda65f7a4fd8d8992dba5c970c3fe10f40d
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2024-04-16 17:40:43 -0700

    mobile menu
    The mobile menu icon now displays with js.

---
+++ main.js
@@ -0,0 +1,5 @@
+// https://www.w3schools.com/howto/howto_css_menu_icon.asp
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementsByTagName("NAV")[0].classList.toggle("change");
+}
